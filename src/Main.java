cc4c0658608dca733ad23374d09f1fa4f7dec54b
Vuspupu1 <hodpopo1@gmail.com>
2026-01-09 15:44:32 +0200

    new program

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        // קבועים - מחירי הנסיעה
+        double FIRST_PRICE = 10.2;       // מחיר ראשוני
+        double PRICE_PER_KM = 1.3;      // מחיר לקילומטר
+        double PRICE_PER_SUITCASE = 2.0; // מחיר למזוודה
+
+        int km;             // כמות קילומטרים
+        int suitcase;       // כמות מזוודות
+        double totalPrice;  // מחיר סופי
+
+        // קלט מהמשתמש
+        System.out.println("Count of KM: ");
+        km = scanner.nextInt();
+
+        System.out.println("Count of suitcases: ");
+        suitcase = scanner.nextInt();
+
+        // חישוב מחיר סופי (תיקון: מזוודות מוכפלות במחיר למזוודה)
+        totalPrice = FIRST_PRICE + (km * PRICE_PER_KM) + (suitcase * PRICE_PER_SUITCASE);
+
+        // הדפסה של מחיר סופי
+        System.out.println("The total price is: " + totalPrice);
+
+        scanner.close(); // סגירת הסורק
     }
 }
